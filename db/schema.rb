8b873f7492c09c384a87838a746099b913eac24a
nickAJ286 <ajiji3f@gmail.com>
2023-10-08 14:52:54 +0900

    データテーブル設定とER図の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_08_023427) do
+ActiveRecord::Schema.define(version: 2023_10_08_042545) do
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "catch_copy", null: false
+    t.text "explanation"
+    t.date "event_date"
+    t.time "event_time"
+    t.string "place"
+    t.string "capacity"
+    t.string "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema.define(version: 2023_10_08_023427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
