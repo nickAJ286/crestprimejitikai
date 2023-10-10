bf65a4b7f0af7d31b65982a74a7a06bac700c956
nickAJ286 <ajiji3f@gmail.com>
2023-10-10 21:30:44 +0900

    approvalコントローラーとビューの作成

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,4 +1,5 @@
 class ApprovalsController < ApplicationController
+
   def index
     @approval = Approval.new
     @event = Event.find(params[:event_id])
@@ -10,9 +11,25 @@ class ApprovalsController < ApplicationController
     redirect_to event_path(params[:event_id])
   end
 
+  def edit
+    @event = Event.find(params[:event_id])
+    @approval = Approval.find_by(user_id: current_user.id, event_id: @event.id)
+  end
+
+  def update
+    @event = Event.find(params[:event_id])
+    @approval = Approval.find(params[:id])
+    if @approval.update(approval_params)
+      redirect_to event_path(@event.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def approval_params
     params.require(:approval).permit(:pros_and_cons_id, :opinion).merge(user_id: current_user.id,event_id: params[:event_id])
   end
+
 end
