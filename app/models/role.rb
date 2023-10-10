22d54508f9205965cb65d51351980a624dd90735
nickAJ286 <ajiji3f@gmail.com>
2023-10-10 12:55:19 +0900

    アクティヴハッシュを利用したroleモデルの作成

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,9 @@
-class Role < ApplicationRecord
-  belongs_to :type
+class Role < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '役員' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
 end
