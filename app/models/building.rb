ec3974ee9db665c081b141645fb4a1f83c45810b
nickAJ286 <ajiji3f@gmail.com>
2023-10-10 12:56:33 +0900

    街区登録の記述調整

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,10 +1,10 @@
 class Building < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '壱番街' },
-    { id: 3, name: '弐番街' },
-    { id: 4, name: '参番街' },
-    { id: 5, name: '七番街' },
+    { id: 0, name: '---' },
+    { id: 1, name: '壱番街' },
+    { id: 2, name: '弐番街' },
+    { id: 3, name: '参番街' },
+    { id: 7, name: '七番街' },
   ]
 
   include ActiveHash::Associations
