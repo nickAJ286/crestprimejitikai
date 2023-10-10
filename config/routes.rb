9aa45c55b73d28fdc80a8ebad8abb777fb6dd6ad
nickAJ286 <ajiji3f@gmail.com>
2023-10-10 12:57:30 +0900

    ルーティングの設定とマイグレーションファイルの微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:index, :edit, :update]
   root to: 'events#index'
   resources :events
-  
 end
--- db/migrate/20231008023427_devise_create_users.rb
+++ db/migrate/20231008023427_devise_create_users.rb
@@ -8,9 +8,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.integer :building_id,       null: false
-      t.integer :room,               null: false
+      t.integer :room,              null: false
       t.string :family_name,        null: false
       t.string :first_name,         null: false
+      t.integer :role_id,           null: false, default:1
 
       ## Recoverable
       t.string   :reset_password_token
