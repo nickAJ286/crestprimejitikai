2b15de2ef53c1d50e82f683026217135d455c142
nickAJ286 <ajiji3f@gmail.com>
2023-10-11 16:18:09 +0900

    時間表示の修正

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,6 @@ module Crestprimejitikai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
