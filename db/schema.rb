e33ef5c04cdcc667a9f69fb853de5217e094ff6a
nickAJ286 <ajiji3f@gmail.com>
2023-10-17 20:49:44 +0900

    マイグレートとルーティングの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_13_060619) do
+ActiveRecord::Schema.define(version: 2023_10_16_143835) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2023_10_13_060619) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "after_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_after_comments_on_event_id"
+    t.index ["user_id"], name: "index_after_comments_on_user_id"
   end
 
   create_table "approvals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -110,6 +120,8 @@ ActiveRecord::Schema.define(version: 2023_10_13_060619) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "after_comments", "events"
+  add_foreign_key "after_comments", "users"
   add_foreign_key "approvals", "events"
   add_foreign_key "approvals", "users"
   add_foreign_key "before_comments", "events"
