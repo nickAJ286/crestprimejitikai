b496d1113ab620f7bd1ea09e63745ac35d8e9556
nickAJ286 <ajiji3f@gmail.com>
2023-11-17 00:05:52 +0900

    beforeコメント

--- app/controllers/before_comments_controller.rb
+++ app/controllers/before_comments_controller.rb
@@ -1,8 +1,11 @@
 class BeforeCommentsController < ApplicationController
   def create
     @before_comment = BeforeComment.new(before_comment_params)
+    @event = Event.find(params[:event_id])
     if @before_comment.save
-      redirect_to event_path(params[:event_id])
+      BeforeCommentChannel.broadcast_to @event, { before_comment: @before_comment, user: @before_comment.user } #追加
+      # ActionCable.server.broadcast "before_comment_channel", {before_comment: @before_comment, user: @before_comment.user} #追加
+      # redirect_to event_path(params[:event_id])
     end
   end
 
